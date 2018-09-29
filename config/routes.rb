f6e51b1b1a719d0d567256dd2454fe679b236377
Martin Carniello <martincarniello@gmail.com>
2018-09-29 15:46:09 -0300

    Versioning API V1

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 	post 'signup', to: 'users#create'
   post 'auth/login', to: 'authentication#authenticate'
 
-  resources :people
-  resources :movies
-  resources :movies_people
+  namespace :v1 do
+	  resources :people
+	  resources :movies
+	  resources :movies_people
+	end
 end
